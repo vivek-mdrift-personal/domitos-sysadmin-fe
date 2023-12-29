647265d5a348741336473522506e4171c813bf11
Vivek M Elayidom <vivek.elayidom@mdrift.com>
2023-12-29 13:28:13 +0530

    test

--- commitlint.config.js
+++ commitlint.config.js
@@ -8,6 +8,7 @@
 // style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
 // test: Adding missing tests or correcting existing tests
 
+
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
